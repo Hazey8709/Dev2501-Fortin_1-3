a8fb36b78b7567e09f7e1cf529772ffb790a95d9
Hazey8709 <jfortin802@gmail.com>
2022-11-26 18:31:38 -0500

    cc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 
+// import Header from "./components/Header"
+
 class App extends Component {
     render() {
         return (
             <div className='App'>
-                <h1>Running! Good luck</h1>
+            <h1>Running! Good luck</h1>
+            <header>
+              {/* <Header /> */}
+            </header>
             </div>
         );
     }
